47cf5cc92cf60bb6203558aa4c27289a66ab7f37
Sanmar Official <sanmaroff@gmail.com>
2025-08-30 10:39:55 +0300

    2.0

--- src/main/java/com/secondchance/SecondChance.java
+++ src/main/java/com/secondchance/SecondChance.java
@@ -1,5 +1,6 @@
 package com.secondchance;
 
+import org.bukkit.Effect;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
@@ -11,8 +12,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionType;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 import org.jetbrains.annotations.NotNull;
@@ -25,12 +29,16 @@ public final class SecondChance extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        //Refister Listener
         getServer().getPluginManager().registerEvents(this, this);
 
+        //Log for start plugin
         getLogger().info("Plugin 'Second Chance' is enable!");
 
+        //Variable for scoreboard manager
         main = getServer().getScoreboardManager().getMainScoreboard();
 
+        //Make sure that team exist
         Team team = main.getTeam("secondChance");
         if (team == null) {
             main.registerNewTeam("secondChance");
@@ -39,6 +47,7 @@ public final class SecondChance extends JavaPlugin implements Listener {
 
     @Override
     public void onDisable() {
+        //Log for disable plugin
         getLogger().info("Plugin 'Second Chance' is disable!");
     }
 
@@ -48,6 +57,8 @@ public final class SecondChance extends JavaPlugin implements Listener {
         if (event.getEntity() instanceof Player player) {
             if (player.getHealth() - event.getFinalDamage() <= 0) {
                 Team team = player.getScoreboard().getEntryTeam(player.getName());
+
+                //Main Function
                 if (getConfig().getBoolean("secondChance")
                         && team != null
                         && team.getName().equalsIgnoreCase("secondChance")) {
@@ -57,10 +68,19 @@ public final class SecondChance extends JavaPlugin implements Listener {
                             player.getLocation(), // где слышно звук
                             Sound.ITEM_TOTEM_USE, // тип звука
                             1.3f, // громкость
-                            1.0f  // высота тона (pitch)
+                            0.5f  // высота тона (pitch)
                     );
-                    Objects.requireNonNull(main.getTeam("secondChance")).removePlayer(player);
+
+                    //Remove Player from Team
+                    team.removePlayer(player);
                     String a = String.valueOf('"');
+
+                    //Effects
+                    player.setAbsorptionAmount(4);
+                    PotionEffect fireResistence = new PotionEffect(PotionType.FIRE_RESISTANCE.getEffectType(), 10, 0, false, false);
+                    player.addPotionEffect(fireResistence);
+
+                    //Send a Message
                     player.sendActionBar(Objects.requireNonNull(getConfig().getString(("secondChanceMessage").replaceAll(a, ""))));
                 }
             }
@@ -71,8 +91,10 @@ public final class SecondChance extends JavaPlugin implements Listener {
 
     @EventHandler
     public void OnPlayerRMB(PlayerInteractEvent event) {
+        //Check main hand
         if (event.getPlayer().getInventory().getItemInMainHand().getType() == Material.TOTEM_OF_UNDYING
                 && (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)) {
+            //Chack team
             if (!main.getTeam("secondChance").hasPlayer(event.getPlayer())) {
                 Objects.requireNonNull(main.getTeam("secondChance")).addPlayer(event.getPlayer());
                 event.getPlayer().getInventory().setItemInMainHand(null);
@@ -81,8 +103,16 @@ public final class SecondChance extends JavaPlugin implements Listener {
         }
     }
 
+    @EventHandler
+    public void OnRespawn(PlayerRespawnEvent event) {
+        //Add player to team on Respawn
+        Player player = event.getPlayer();
+        main.getTeam("secondChance").addPlayer(player);
+    }
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String[] args) {
+        //Commands
         if (command.getName().equalsIgnoreCase("SecondChance")) {
             if (args.length == 0) {
                 sender.sendMessage("Second Chance value : " + getConfig().getBoolean("secondChance"));
